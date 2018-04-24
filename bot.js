8b528c409a559c403bef225ecf31078ada59c191
James Byrnes <mail@jamesbyrnes.ca>
2018-04-24 00:13:58 -0400

    Build out the message handler

--- bot.js
+++ bot.js
@@ -3,26 +3,14 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const dclient = new Discord.Client();
 
-const { getRandomLyric } = require('./models/lyrics');
+const { messageHandler } = require('./handlers/messageHandler');
 
 dclient.on('ready', () => {
   console.log(`Logged in as ${dclient.user.tag}!`);
 });
 
 dclient.on('message', (msg) => {
-  if (msg.author.id === dclient.user.id) return;
-
-  if (msg.content === '<:ronan:422580945250549766>') {
-    getRandomLyric()
-    .then((lyrics) => {
-      msg.reply(lyrics);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  } else if (msg.content.indexOf('⚔') >= 0) {
-    msg.reply("SWOOOOORDS");
-  }
+  messageHandler(dclient, msg);
 });
 
 dclient.login(process.env.DISCORD_CLIENT_TOKEN);
---
+++ handlers/messageHandler.js
@@ -0,0 +1,25 @@
+const settings = require('../settings');
+const { getRandomLyric } = require('../models/lyrics');
+
+module.exports.messageHandler = (dclient, msg) => {
+  const mainSettings = settings.getSettings();
+  const responseTable = settings.getInverseResponseTable(); 
+
+  if (msg.author.id === dclient.user.id) return;
+  if (Object.keys(responseTable).indexOf(msg.content) < 0) return;
+  if (mainSettings.buildVersion === "dev" && mainSettings.devChannels.indexOf(msg.channel.name) < 0) return;
+
+  const trigger = responseTable[msg.content];
+
+  if (trigger === "LYRIC_RANDOM") {
+    getRandomLyric()
+    .then((lyrics) => {
+      msg.reply(lyrics);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  } else if (trigger === "BANTER_SWORDS") {
+    msg.reply("SWOOOOOOORDS");
+  }
+};
