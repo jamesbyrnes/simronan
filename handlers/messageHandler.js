04dd776dea855f8aeb7736ba7a79ac09580c84c5
James Byrnes <mail@jamesbyrnes.ca>
2020-04-30 21:45:24 -0400

    Fix settings- Remove required settings file- Hardcode trigger values- Make easter egg more better

--- handlers/messageHandler.js
+++ handlers/messageHandler.js
@@ -1,20 +1,12 @@
-const settings = require('../settings');
 const { getRandomLyric, getAllLyrics } = require('../models/lyrics');
 const Markov = require('../models/markov');
 
 module.exports.messageHandler = (dclient, msg) => {
-  const mainSettings = settings.getSettings();
-  const responseTable = settings.getInverseResponseTable(); 
-
   if (msg.author.id === dclient.user.id) return;
-  if (Object.keys(responseTable).indexOf(msg.content) < 0) return;
-  if (mainSettings.channels.length > 0) {
-    if (mainSettings.channels.indexOf(msg.channel.name) < 0) return;
-  }
 
-  const trigger = responseTable[msg.content];
+  const trigger = msg.content;
 
-  if (trigger === "LYRIC_RANDOM") {
+  if (trigger === "<:ronan:422580945250549766>") {
     getRandomLyric()
     .then((lyrics) => {
       msg.reply(lyrics);
@@ -22,7 +14,7 @@ module.exports.messageHandler = (dclient, msg) => {
     .catch((err) => {
       console.log(err);
     });
-  } else if (trigger === "LYRIC_MARKOV") {
+  } else if (trigger === "<:notafraid:422937125370069002>") {
     getAllLyrics()
     .then((lyricsArray) => {
       markovChain = new Markov(lyricsArray);
@@ -31,7 +23,7 @@ module.exports.messageHandler = (dclient, msg) => {
     .catch((err) => {
       console.log(err);
     });
-  } else if (trigger === "BANTER_SWORDS") {
+  } else if (trigger.includes("swords")) {
     msg.reply("SWOOOOOOORDS");
   } 
 };
